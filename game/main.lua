e801d69c5679331e13570b96d6cbe4b741395f17
Lee2121 <lcudney@yahoo.com>
2025-12-30 23:10:51 -0500

    Spawning 1 mil pixels

--- game/main.lua
+++ game/main.lua
@@ -1,16 +1,23 @@
 HumpCamera = require "lib.hump.camera"
 BackgroundGrid = require "src.framework.grid.backgroundGrid"
 
+PixelManager = require "src.framework.pixels.pixelManager"
+
 GameCamera = {}
 
 function love.load()
 	love.graphics.setDefaultFilter( 'nearest', 'nearest' )
 	GameCamera = HumpCamera(0, 0)
 	BackgroundGrid:init()
+
+	PixelManager:init()
+	PixelManager:spawnPixels(1000000)
 end
 
 function love.update(dt)
-	GameCamera:move(1, 1)
+	
+
+	--GameCamera:move(1, 1)
 end
 
 function love.draw()
@@ -20,9 +27,11 @@ function love.draw()
 	GameCamera:attach()
 
 	-- draw game objects here
-	love.graphics.rectangle("fill", 0, 0, 1, 1)
+	--love.graphics.rectangle("fill", 0, 0, 1, 1)
+	PixelManager:draw()
 
 	GameCamera:detach()
 
-	-- draw UI her
+
+	love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 end
--- game/src/framework/pixels/pixelManager.lua
+++ game/src/framework/pixels/pixelManager.lua
@@ -1,13 +1,40 @@
 local PixelManager = {
-	pixelColors = {},
-	pixelLocations = {},
-	pixelBehaviors = {}
+	pixels = {}, -- location and color
+	pixelBehaviors = {},
 }
+
+function PixelManager:init()
+	-- init pixel colors
+	self.possiblePixelColors = {
+		{ 1, 0, 0, 1 }, -- red
+		{ 0, 1, 0, 1 }, -- green
+		{ 0, 0, 1, 1 } -- blue
+	}
+end
 
 function PixelManager:spawnPixels(num)
 
+	for i = 1, num, 1 do
+		-- location
+		local locX, locY = math.random(love.graphics.getWidth()), math.random(love.graphics.getHeight())
+		
+		-- color
+		local randColorIndex = math.random(#self.possiblePixelColors)
+		local randColor = self.possiblePixelColors[randColorIndex]
+		
+		-- behavior
+
+		local newPixel = { locX, locY, unpack(randColor) }
+		table.insert(self.pixels, newPixel)
+	end
 end
 
+function PixelManager:update(dt)
 
+end
+
+function PixelManager:draw()
+	love.graphics.points(self.pixels)
+end
 
 return PixelManager
